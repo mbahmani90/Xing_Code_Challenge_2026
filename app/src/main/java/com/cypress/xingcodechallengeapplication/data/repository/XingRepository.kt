3a0142441a7f6327461c20d4a0b6cf4d098f79fd
Majid <majidbahmaniapp@gmail.com>
2026-02-07 11:10:11 +0000

    1) Developed XingRepository.kt to provide pager for the viewModel.

---
+++ app/src/main/java/com/cypress/xingcodechallengeapplication/data/repository/XingRepository.kt
@@ -0,0 +1,39 @@
+package com.cypress.xingcodechallengeapplication.data.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.room.Entity
+import com.cypress.xingcodechallengeapplication.data.local.XingDataBase
+import com.cypress.xingcodechallengeapplication.data.local.XingEntity
+import com.cypress.xingcodechallengeapplication.data.remote.XingClientApi
+import com.cypress.xingcodechallengeapplication.data.remote.XingClientApi.Companion.PAGE_SIZE
+import com.cypress.xingcodechallengeapplication.data.remote.XingRemoteMediator
+
+interface XingRepository {
+    fun getXingPager() : Pager<Int , XingEntity>
+}
+
+@OptIn(ExperimentalPagingApi::class)
+class XingRepositoryImp(
+    private val xingClientApi: XingClientApi,
+    private val xingDataBase: XingDataBase
+): XingRepository{
+
+
+    override fun getXingPager(): Pager<Int, XingEntity> {
+
+        return Pager(
+            config = PagingConfig(pageSize = PAGE_SIZE),
+            remoteMediator = XingRemoteMediator(
+                xingClientApi , xingDataBase
+            ),
+            pagingSourceFactory = {
+                xingDataBase.xingDao.getReposPagingSource()
+            }
+        )
+
+    }
+
+
+}
